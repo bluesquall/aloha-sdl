7b77977e521a9c5d721c8cd42a69e985304cc3b5
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-08-25 15:12:43 -0700

    actually use SDL
    - add code from [example][e]
    
    [e]: https://mesonbuild.com/GuiTutorial.html#upgrading-the-program-to-use-sdl

--- src/aloha-sdl.c
+++ src/aloha-sdl.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "SDL.h"
+
 int main(int argc, char **argv) {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_Surface *surface;
+  SDL_Event *event;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize SDL: %s", SDL_GetError());
+    return 3;
+  }
+
+  if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not create window and renderer: %s", SDL_GetError());
+    return 3;
+  }
+
   printf("Aloha from SDL2!\n");
+
+  while (1) {
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT) {
+      break;
+    }
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
   return EXIT_SUCCESS;
 }
